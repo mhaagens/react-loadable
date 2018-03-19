958227303be9b7e6c718c3442c92f48694ab643b
mhaagens <martin.haagensli@gmail.com>
2018-03-19 09:30:24 +0100

    update webpack

--- src/webpack.js
+++ src/webpack.js
@@ -1,21 +1,27 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
 
 function buildManifest(compiler, compilation) {
   let context = compiler.options.context;
   let manifest = {};
 
-  compilation.chunks.forEach(chunk => {
-    chunk.files.forEach(file => {
-      chunk.forEachModule(module => {
+  for (const chunk of compilation.chunks) {
+    for (const file of chunk.files) {
+      for (const module of chunk.modulesIterable) {
         let id = module.id;
-        let name = typeof module.libIdent === 'function' ? module.libIdent({ context }) : null;
-        let publicPath = url.resolve(compilation.outputOptions.publicPath || '', file);
-        
+        let name =
+          typeof module.libIdent === "function"
+            ? module.libIdent({ context })
+            : null;
+        let publicPath = url.resolve(
+          compilation.outputOptions.publicPath || "",
+          file
+        );
+
         let currentModule = module;
-        if (module.constructor.name === 'ConcatenatedModule') {
+        if (module.constructor.name === "ConcatenatedModule") {
           currentModule = module.rootModule;
         }
         if (!manifest[currentModule.rawRequest]) {
@@ -23,9 +29,9 @@ function buildManifest(compiler, compilation) {
         }
 
         manifest[currentModule.rawRequest].push({ id, name, file, publicPath });
-      });
-    });
-  });
+      }
+    }
+  }
 
   return manifest;
 }
@@ -36,20 +42,23 @@ class ReactLoadablePlugin {
   }
 
   apply(compiler) {
-    compiler.plugin('emit', (compilation, callback) => {
-      const manifest = buildManifest(compiler, compilation);
-      var json = JSON.stringify(manifest, null, 2);
-      const outputDirectory = path.dirname(this.filename);
-      try {
-        fs.mkdirSync(outputDirectory);
-      } catch (err) {
-        if (err.code !== 'EEXIST') {
-          throw err;
+    compiler.hooks.emit.tapAsync(
+      "ReactLoadablePlugin",
+      (compilation, callback) => {
+        const manifest = buildManifest(compiler, compilation);
+        var json = JSON.stringify(manifest, null, 2);
+        const outputDirectory = path.dirname(this.filename);
+        try {
+          fs.mkdirSync(outputDirectory);
+        } catch (err) {
+          if (err.code !== "EEXIST") {
+            throw err;
+          }
         }
+        fs.writeFileSync(this.filename, json);
+        callback();
       }
-      fs.writeFileSync(this.filename, json);
-      callback();
-    });
+    );
   }
 }
 
